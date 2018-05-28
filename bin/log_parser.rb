768a6e64a749e1ebcf0d18e29fb377de1a71289d
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-05-28 13:32:48 +0300

    add spec for succesfully parsing a weblog line and make it pass

--- bin/log_parser.rb
+++ bin/log_parser.rb
@@ -3,5 +3,10 @@ class LogParser
   def parse(log: '')
     return false unless log.is_a? String
     return false if log.empty?
+    data = log.match(/(?<web_page>.*)\s(?<user_ip>\d{3}\.\d{3}\.\d{3}\.\d{3})/)
+    {
+        page: data[:web_page],
+        user: data[:user_ip]
+    }
   end
 end
--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe LogParser do
   it 'returns false if anything else except a string is passed in' do
     expect(parser.parse(log: nil)).to eq false
   end
+
+  it 'returns false with an empty log passed in' do
+    expect(parser.parse(log: '')).to eq false
+  end
+
+  it 'parses correctly a log line' do
+    line_to_parse = '/help_page/1 126.318.035.038'
+    result = { page: '/help_page/1', user: '126.318.035.038' }
+    expect(parser.parse(log: line_to_parse)).to eq result
+  end
 end
