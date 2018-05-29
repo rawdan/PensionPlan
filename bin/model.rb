b59afa8088e6ce867aa2460403945deb695b7b0a
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-05-29 16:33:28 +0300

    add test for getting relevant parsed results from file and make them pass

--- bin/model.rb
+++ bin/model.rb
@@ -1,5 +1,34 @@
+require_relative 'log_parser'
+require 'set'
+
 class Model
+  attr_reader :log_parser, :file, :data
+
   def the_truth
     true
+  end
+
+  def initialize(log_parser: LogParser.new, file: false)
+    @log_parser = LogParser.new
+    @file = file
+    get_data
+  end
+
+  def get_data
+    @data = {}
+    return data unless file
+    file.each do |log_line|
+      line_data = log_parser.parse log: log_line
+      if data[line_data[:page]]
+        @data[line_data[:page]][:visits] += 1
+        @data[line_data[:page]][:visitors] << line_data[:user]
+      else
+        @data[line_data[:page]] = {
+            visits: 1,
+            visitors: Set.new([line_data[:user]])
+        }
+      end
+    end
+    @data
   end
 end
--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -1,8 +1,35 @@
 require 'spec_helper'
+require 'tempfile'
 require_relative '../bin/model'
+require_relative '../bin/log_parser'
 
 RSpec.describe Model do
-  it "should be true" do
-   expect(true).to be true
+  it 'should be true' do
+    expect(true).to be true
+  end
+
+  it 'can initialize with the parser' do
+    results = Model.new(log_parser: LogParser.new)
+    expect(results.log_parser).to be_a LogParser
+  end
+
+  it 'can initialize with a file object' do
+    results = Model.new file: File.open(File.join(Dir.pwd, 'webserver.log'), 'r')
+    expect(results.file).to be_a File
+  end
+
+  it 'initializes the data after reading the file' do
+    Tempfile.create('log') do |f|
+      f.puts '/home 184.123.665.067'
+      f.puts '/about/2 444.701.448.104'
+      f.puts '/about/2 444.701.448.104'
+      f.rewind
+      expected_results = {
+          "/about/2" => { visits: 2, visitors: Set.new(['444.701.448.104'])},
+          "/home" => { visits: 1, visitors: Set.new(['184.123.665.067'])}
+      }
+      results = Model.new file: f
+      expect(results.data).to eq expected_results
+    end
   end
 end
