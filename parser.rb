f5f145e14bd1fc0e3d0acb08ddc2a4cb4b108c4e
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-05-29 18:00:58 +0300

    finish up

--- parser.rb
+++ parser.rb
@@ -1,3 +1,13 @@
 #!/usr/bin/env ruby
-file_name = ARGV.first
-puts file_name
+require_relative 'bin/model'
+
+file = File.open(ARGV.first)
+results = Model.new file: file
+puts 'Webpages ordered by page views descending:'
+puts results.order_by_page_views
+puts 'Webpages ordered by unique page views descending:'
+puts results.order_by_unique_page_views
+
+# command: ./parser.rb webserver.log
+# or
+# ruby parser.rb webserver.log
